4e55c1a43e3bb80c7e791891951a975e30252bee
renney <renney003@gmail.com>
2025-07-10 11:10:11 +0300

    chore: Set is_superuser to True when creating a user

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -37,7 +37,7 @@ class CustomUserManager(BaseUserManager):
 
         user.set_password(password)
         extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_superuser", True)
         user.save(using=self._db)
         return user
     
